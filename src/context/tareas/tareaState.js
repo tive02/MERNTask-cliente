95fe65391e8166533b563f1901b1701f1ab3685a
Jorge Moreno <jsmorenop@unal.edu.co>
2021-04-02 09:58:20 -0500

    Obteniendo las tareas del proyecto

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -23,11 +23,18 @@ const TareaState = (props) => {
 
   //Crear las funciones
   //obtener las tareas de un proyecto
-  const obtenerTareas = (proyectoId) => {
-    dispatch({
-      type: TAREAS_PROYECTO,
-      payload: proyectoId,
-    });
+  const obtenerTareas = async (proyecto) => {
+    try {
+      const resultado = await clienteAxios.get("/api/tareas", {
+        params: { proyecto },
+      });
+      dispatch({
+        type: TAREAS_PROYECTO,
+        payload: resultado.data.tareas,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
   //Agregar las tareas
   const agregarTarea = async (tarea) => {
